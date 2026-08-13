dc5aa1cac973d5cd730c14def83b88a7f967f80d
Ranveer Singh <ranveers3795@gmail.com>
2026-08-13 16:12:04 +0530

    Student_Record_Management.c

--- Student_Record_Management.c
+++ Student_Record_Management.c
@@ -47,11 +47,102 @@ struct Student
     float percentage;
 };
 
-/*----------------------------------------------
+/*-----------------------------------------------
                 Global Variable
 -------------------------------------------------*/ 
 
 struct Student student[MAX] ;
 int totalStudents = 0;
 
-/**/
+/*--------------------------------------------
+            Function Declarations
+----------------------------------------------*/
+float calculatePercentage(float marks1,float marks2,float marks3);
+
+void addStudent();
+void displayStudents();
+
+void searchStudent();
+void updateStudent();
+void deleteStudent();
+
+/*--------------------------------------------
+            Calculate Percentage
+----------------------------------------------*/
+float calculatePercentage(float marks1,float marks2,float marks3)
+{
+    return(marks1 + marks2 + marks3) / 3.0;
+}
+
+/*--------------------------------------------
+        Add Student
+----------------------------------------------*/
+void addStudent()
+{
+    printf("\n********************************\n");
+    printf("\t\t ADD STUDENT RECORD\n");
+    printf("********************************\n");
+
+    printf("Enter Roll Number : "); // Input roll number of student
+    scanf("%d", &student[totalStudents].rollno);
+
+    printf("Enter Name : "); // Input name of student
+    scanf(" %[^\n]", student[totalStudents].name); // To read string with spaces
+
+    printf("Enter Age : "); // Input age of student
+    scanf("%d", &student[totalStudents].age);
+
+    printf("Enter Department : "); // Input department of student
+    scanf(" %[^\n]", student[totalStudents].department);
+
+    printf("Enter Marks of Subject 1 : "); // Input marks of subject 1
+    scanf("%f", &student[totalStudents].marks1);
+
+    printf("Enter Marks of Subject 2 : "); // Input marks of subject 2
+    scanf("%f", &student[totalStudents].marks2);
+
+    printf("Enter Marks of Subject 3 : "); // Input marks of subject 3
+    scanf("%f", &student[totalStudents].marks3);
+    
+    student[totalStudents].percentage =
+     calculatePercentage(student[totalStudents].marks1, student[totalStudents].marks2, student[totalStudents].marks3);
+
+     totalStudents++; // Increment total number of students
+
+     printf("\nStudent Record Added Successfully!\n");
+}
+
+/*----------------------------------------------------
+        Display Students
+------------------------------------------------------*/
+void displayStudents()
+{
+    int i;// Loop variable
+
+    if(totalStudents == 0)
+    {
+        printf("\n No Student Records Found ! \n");
+        return;
+    }
+
+    printf("\n********************************\n");
+    printf("\t\t STUDENT RECORDS\n");
+    printf("********************************\n");
+
+    for(i = 0; i<totalStudents; i++)
+    {
+        printf("\n Roll Number : %d \n", student[i].rollno); 
+        printf("\n Name : %s \n", student[i].name);
+        printf("\n Age : %d \n", student[i].age);
+        printf("\n Department : %s \n", student[i].department);
+
+        printf("\n Marks of Subject 1 : %.2f \n", student[i].marks1);
+        printf("\n Marks of Subject 2 : %.2f \n", student[i].marks2);
+        printf("\n Marks of Subject 3 : %.2f \n", student[i].marks3);
+
+        printf("\n Percentage : %.2f \n", student[i].percentage);
+    }
+
+    printf("\n********************************\n");
+
+}
