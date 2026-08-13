da4c9c13b88b9d352dc3622056da8cc398302aed
Ranveer Singh <ranveers3795@gmail.com>
2026-08-13 16:41:10 +0530

    Student_Record_Management.c

--- Student_Record_Management.c
+++ Student_Record_Management.c
@@ -146,3 +146,62 @@ void displayStudents()
     printf("\n********************************\n");
 
 }
+
+/*--------------------------------------------
+        Main Function 
+----------------------------------------------*/
+
+int main()
+{
+    int choice; // Variable to store user choice
+
+    while(1) //Infinite loop to display menu until user chooses to exit
+    {
+        printf("\n********************************\n");
+        printf("\t STUDENT RECORD MANAGEMENT SYSTEM \n");
+        printf("********************************\n");
+        
+        printf("1. Add Student\n");
+        printf("2. Display Students \n");
+        printf("3. Search Student \n");
+        printf("4. Update Student \n");
+        printf("5. Delete Student \n ");
+        printf("6. Exit \n");
+        printf("\n ************************************\n");
+
+        printf("Enter your choice : ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+            case 1:
+            addStudent();
+            break;
+
+            case 2:
+            displayStudents();
+            break;
+
+            case 3:
+            searchStudent();
+            break;
+
+            case 4:
+            updateStudent();
+            break;
+
+            case 5:
+            deleteStudent();
+            break;
+
+            case 6:
+            printf("\n Thank You for using Student Record Management System. Goodbye! \n");
+            return 0;
+
+            default:
+            printf("\n Invalid Choice! Please try again. \n");
+        }
+    }
+
+    return 0;
+}
